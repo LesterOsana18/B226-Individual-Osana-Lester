5e97865137df991d47e50eb9b23449d549c5dced
LesterOsana18 <lstrosana.18@gmail.com>
2026-06-20 17:37:28 +0800

    Retry test.

--- src/day3/discussion/NewClass.java
+++ src/day3/discussion/NewClass.java
@@ -3,7 +3,6 @@ package day3.discussion;
 public class NewClass {
 	
 	public static void Main(String[] args) {
-		System.out.println("Hello, World!");
-		System.out.println("This is a practice test.");
+		System.out.println("Retry!");
 	}
 }
